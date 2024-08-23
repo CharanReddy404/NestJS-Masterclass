a55343bb1793cb81d57feef54567e438b8e092ba
charanreddy404 <charanlucky6143@gmail.com>
2024-08-24 04:47:50 +0530

    03 Validation and Pipes

--- nestjs-intro/src/users/users.controller.ts
+++ nestjs-intro/src/users/users.controller.ts
@@ -1,32 +1,40 @@
 import {
   Body,
   Controller,
+  DefaultValuePipe,
   Get,
-  Headers,
-  Ip,
   Param,
+  ParseIntPipe,
+  Patch,
   Post,
   Query,
 } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { GetUsersoParamDto } from './dtos/get-users-param.dto';
+import { PatchUsersDto } from './dtos/patch-users.dto';
 
 @Controller('users')
 export class UsersController {
-  @Get('/:id/:optional?')
-  public getUsers(@Param('id') id: any, @Query('limit') limit: any) {
-    console.log(id);
+  @Get('/:id?')
+  public getUsers(
+    @Param() getUserParamDto: GetUsersoParamDto,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number,
+  ) {
+    console.log(getUserParamDto);
     console.log(limit);
+    console.log(page);
     return 'you send a GET request to users endpoint';
   }
 
   @Post()
-  public createUser(
-    @Body() request: any,
-    @Headers() headers: any,
-    @Ip() ip: any,
-  ) {
-    console.log(request);
-    console.log(headers);
-    console.log(ip);
+  public createUser(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto);
     return 'you send a POST request to users endpoint';
+  }
+
+  @Patch()
+  public patchUser(@Body() patchUserDto: PatchUsersDto) {
+    return patchUserDto;
   }
 }
